0d9115afcc2346bcaaa83de16b9560f7de50ad6f
andrianazaliznak <andrianazaliznyak@gmail.com>
2024-05-07 20:12:28 +0300

    updated

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,17 +1,16 @@
 import React, {useEffect, useState} from 'react';
-import {SubmitHandler, useForm} from 'react-hook-form';
-import {useSearchParams} from "react-router-dom";
 
-import css from "../components/MoviesContainer/MoviesList.module.css";
-import {useAppDispatch, useAppSelector} from "../hooks";
-import {moviesActions} from "../redux";
-import {MovieSearch} from "../components";
+
+
+
+
+
 import {PeopleSearch} from "../components/PeopleContainer/PeopleSearch";
 
 
 
 const SearchPage = () => {
-    const dispatch = useAppDispatch()
+
 
     const [query, setQuery] = useState('');
 
@@ -32,11 +31,11 @@ const SearchPage = () => {
 
 
     return (
-        <div className={css.SearchPage}>
-                <input className={css.input} type="text" placeholder={'Search film'} value={query}
+        <div >
+                <input  type="text" placeholder={'Search film'} value={query}
                        onChange={handleInputChange}/>
 
-                <button className={css.button} onClick={handleSearch}>Search</button>
+                <button  onClick={handleSearch}>Search</button>
 
 
             <PeopleSearch/>
